250e0138349f7e347038e284ed8f767db21e403d
dorian <dorianlegan@gmail.com>
2021-07-10 12:05:26 +0100

    popout sidebar added along with initial javascript functions

--- script.js
+++ script.js
@@ -1,25 +1,55 @@
 let myLibrary = [];
+let menu = document.querySelector("#menu");
+let sideBar = document.querySelector("#sideBar")
+menu.addEventListener("click", toggleSidebar);
+let form = document.querySelector("#bookInfo");
+form.addEventListener("submit", formSubmitted);
 
-function Book(title, author, pages, read) {
+
+function Book(title, author, pages) {
 
     this.title = title;
     this.pages = pages;
     this.author = author;
-    this.read = read;
-  
-  }
-  
-  book.prototype.info = function() {
-  
-    return this.title + ", " + this.pages + " pages, " + this.read + "."
-  }
 
-  function addBookToLIbrary(title, author, pages, read) {
 
-    let book = new Book(title, author, pages, read);
+}
+
+Book.prototype.info = function () {
+
+    return this.title + ", " + this.pages + " pages";
+}
+
+function addBookToLIbrary(title, author, pages) {
+
+    let book = new Book(title, author, pages);
     myLibrary.push(book);
+    console.log(myLibrary);
     return;
 
-  }
+}
 
-  
+function toggleSidebar(e) {
+
+    console.log(sideBar);
+
+
+    let body = document.querySelector("#wrapper");
+    body.classList.toggle("bodyToggle");
+    let display = document.querySelector("#bookDisplay");
+    display.classList.toggle("bookDisplayToggle");
+    sideBar.classList.toggle("sideBarToggle");
+}
+
+function formSubmitted(e) {
+
+    console.log(e);
+    let title = form.elements[0].value;
+    let author = form.elements[1].value;
+    let length = form.elements[2].value;
+
+    addBookToLIbrary(title, author, length);
+    e.preventDefault();
+
+
+}
